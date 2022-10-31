08adb08138d21cbff6a66c74d5047658212e680d
Wongbuer <wongbuer@outlook.com>
2022-10-31 17:28:34 +0800

    解决vercel跨域问题

--- api/proxy.js
+++ api/proxy.js
@@ -3,16 +3,16 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-    let target = ''
+    // let target = ''
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://baidu.com
-    if (req.url.startsWith('/api')) {
-        target = 'http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx'
-    }
+    // if (req.url.startsWith('/api')) {
+    //     target = ''
+    // }
     // 创建代理对象并转发请求
     createProxyMiddleware({
-        target,
+        target: 'http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx',
         changeOrigin: true,
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/backend`
